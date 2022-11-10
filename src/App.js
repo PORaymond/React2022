30c20dce475a1cbbd22c06735964f5707e04714a
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-10 14:35:55 -0500

    Appeler component externe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Recherche from "./Recherche";
 function App() {
   return (
     <div className="App">
       <h1>Lab 01 Semantic UI React</h1>
+        <Recherche />
     </div>
   );
 }
