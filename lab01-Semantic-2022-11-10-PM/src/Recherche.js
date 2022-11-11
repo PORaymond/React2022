587144e318c6ee57ccb64b44c6b5308c3b16cf1d
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-10 20:12:09 -0500

    Déplacer le projet dans un sous-dossier

---
+++ lab01-Semantic-2022-11-10-PM/src/Recherche.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import {Button, Select} from "semantic-ui-react";
+class Recherche extends Component {
+
+    state = { depart: "", categorie:"" };
+
+    onDepartementChange = (e, data) => {this.setState({depart:data.value})};
+    onCategorieChange = (e, data) => {this.setState({categorie:data.value})};
+
+    render(){
+        console.log(this.state.depart, this.state.categorie);
+        const optionsDpt = [
+            { value: "44", key: "44", text:"Loire Atlantique"},
+            { value: "49", key: "49", text:"Maine et Loire"},
+            { value: "53", key: "53", text:"Mayenne"},
+            { value: "72", key: "72", text:"Sarthe"},
+            { value: "85", key: "85", text:"Vendée"},
+        ];
+
+
+        const optionsType = [
+            { value: "cpam", key:"cpam", text: "Caisse primaire d'assurance maladie"},
+            { value: "cci", key:"cci", text: "Chambre de commerce et d'industrie"},
+            { value: "crous", key:"crous", text: "Crous et ses antennes"}
+        ]
+
+
+        return(
+            <div className="recherche">
+                <Select placeholder="Choisissez un département" options={optionsDpt} value = {this.state.depart} onChange={this.onDepartementChange} />
+                <Select placeholder="Choisissez une administration"  options={optionsType}  value= {this.state.categorie} onChange={this.onCategorieChange}/>
+                <Button primary> Lancer la recherche </Button>
+                <Button secondary> Vider la recherche </Button>
+
+
+            </div>
+        )
+    }
+}
+export default Recherche;
