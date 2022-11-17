661629a5370804a36bb32dd7fb8990fd65dd14bf
Olivier Raymond <olivier.raymond176@gmail.com>
2022-11-17 10:39:12 -0500

    Transformer la fonction en classe pour ajouter le state

--- lab01-Semantic-2022-11-10-PM/src/App.js
+++ lab01-Semantic-2022-11-10-PM/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Recherche from "./Recherche";
-function App() {
-  return (
-    <div className="App">
-      <h1>Lab 01 Semantic UI React</h1>
-        <Recherche />
-    </div>
-  );
+import Recherche from "./Composants/Recherche";
+import {Component} from "react";
+
+class App extends Component {
+
+   state = {}
+
+   render() {
+      return (<div className="App">
+         <h1>Lab 01 Semantic UI React</h1>
+         <Recherche/>
+      </div>);
+   }
 }
 
 export default App;
