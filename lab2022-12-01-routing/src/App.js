21e5b67da0495e3f945a19c58727c2020db0beb1
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-01 10:19:55 -0500

    Ajouter le nouveau projet

---
+++ lab2022-12-01-routing/src/App.js
@@ -0,0 +1,42 @@
+import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import './App.css';
+
+const Accueil = () => { return (<div>Je suis Accueil</div>) };
+const Faq = (props) => {
+    //console.log(props);
+    return (
+        <div>
+            Je suis la foire aux questions
+        
+        </div>)
+
+
+};
+const Cgv = (props) => {
+    return (
+        <div>
+            Je suis les conditions générales de ventes
+        </div>)
+};
+const Page404 = () => {
+   return( <div> Page 404</div>)
+};
+
+
+function App() {
+    return (
+        
+            <BrowserRouter>
+                <Switch>
+
+                    <Route path="/" component={Accueil} exact />
+                    <Route path="/faq" component={Faq} />
+                    <Route path="/cgv" component={Cgv} />
+                    <Route path="*" component={Page404} />
+
+                </Switch>
+            </BrowserRouter>
+       
+    );
+}
+export default App;
--- lab_02_routing/src/App.js
+++ lab_02_routing/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import {BrowserRouter, Route, Router} from "react-router-dom";
+
+const QatarEtatVoyou = () => {
+   return (
+      <div>
+         Page de l’État voyou !
+      </div>
+   )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Lab routing</h1>
-    </div>
-  );
+   return (
+      <Router>
+         <BrowserRouter>
+            <Route path="/fifa" component={QatarEtatVoyou}></Route>
+         </BrowserRouter>
+      </Router>
+   );
 }
 
 export default App;
