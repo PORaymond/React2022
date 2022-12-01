c49d855a52e1c6868a93f4842663df89e49cf6ab
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-01 10:49:48 -0500

    Changer le component en element

--- lab2022-12-01-routing/src/App.js
+++ lab2022-12-01-routing/src/App.js
@@ -1,42 +1,42 @@
-import { BrowserRouter, Route, } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 
-const Accueil = () => { return (<div>Je suis Accueil</div>) };
+const Accueil = () => {
+   return (<div>Je suis Accueil</div>)
+};
 const Faq = (props) => {
-    //console.log(props);
-    return (
-        <div>
-            Je suis la foire aux questions
-        
-        </div>)
+   //console.log(props);
+   return (
+      <div>
+         Je suis la foire aux questions
+
+      </div>)
 
 
 };
 const Cgv = (props) => {
-    return (
-        <div>
-            Je suis les conditions générales de ventes
-        </div>)
+   return (
+      <div>
+         Je suis les conditions générales de ventes
+      </div>)
 };
 const Page404 = () => {
-   return( <div> Page 404</div>)
+   return (<div> Page 404</div>)
 };
 
 
 function App() {
-    return (
-        
-            <BrowserRouter>
-                <Routes>
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Accueil />} />
+            <Route path="/faq" element={<Faq />}/>
+            <Route path="/cgv" element={<Cgv />}/>
+            <Route path="*" element={<Page404 />}/>
+         </Routes>
+      </BrowserRouter>
 
-                    <Route path="/" component={Accueil} exact />
-                    <Route path="/faq" component={Faq} />
-                    <Route path="/cgv" component={Cgv} />
-                    <Route path="*" component={Page404} />
-
-                </Routes>
-            </BrowserRouter>
-       
-    );
+   );
 }
+
 export default App;
