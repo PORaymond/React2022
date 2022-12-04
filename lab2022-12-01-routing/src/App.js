1e938b944aaa5cc175ac26994e4bbc4b78525fd1
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-04 08:22:45 -0500

    Afficher les props d’un composant routé vs non routé

--- lab2022-12-01-routing/src/App.js
+++ lab2022-12-01-routing/src/App.js
@@ -2,20 +2,24 @@ import {BrowserRouter, NavLink, Redirect, Route, Switch} from 'react-router-dom'
 import './App.css';
 import {useState} from "react";
 
-const Accueil = () => { return (<div>Accueil</div>) };
+const Accueil = () => {
+   return (<div>Accueil</div>)
+};
 const Faq = (props) => {
-    //console.log(props);
-    return (
-        <div>
-            La foire aux questions
-        </div>)
+   console.log("Les props du composant Faq :");
+   console.log(props);
+   return (
+      <div>
+         La foire aux questions
+      </div>)
 };
 
 const Cgv = (props) => {
-    return (
-        <div>
-            Conditions générales de ventes
-        </div>)
+   console.log(props);
+   return (
+      <div>
+         Conditions générales de ventes
+      </div>)
 };
 const Page404 = () => {
    const [connected, setConnected] = useState(false);
@@ -23,37 +27,47 @@ const Page404 = () => {
    if (!connected) {
       return (<div>
          <p>Vous n'êtes pas connectés ! </p>
-         <button onClick={() => setConnected(true)}> Se connecter </button>
+         <button onClick={() => setConnected(true)}> Se connecter</button>
       </div>)
    }
 
-   return(
+   return (
       <Redirect to={'/'}/>
    )
 };
 
+const CheckProps = (props) => {
+   console.log(props);
+   return (
+      <div>Vérifier les props de CheckProps</div>
+   )
+};
+   function App() {
+      return (
 
-function App() {
-    return (
-        
-            <BrowserRouter>
-               <header>
-                  <h2>Menu</h2>
-                  <ul>
-                     <li><NavLink to="/" activeClasName="lien-actif" activeStyle={{color:"red", fontWeight:"bold"}}>Accueil</NavLink></li>
-                     <li> <NavLink to='/faq' activeClassName="lien-actif" activeStyle={{color:"red", fontWeight:"bold"}}> F.A.Q </NavLink></li>
-                     <li> <NavLink to='/cgv' activeClassName="lien-actif" activeStyle={{color:"red", fontWeight:"bold"}}> Conditions de vente </NavLink></li>
-                  </ul>
-               </header>
-                <Switch>
+         <BrowserRouter>
+            <CheckProps qqchose="abc"/>
+            <header>
+               <h2>Menu</h2>
+               <ul>
+                  <li><NavLink to="/" activeClasName="lien-actif"
+                               activeStyle={{color: "red", fontWeight: "bold"}}>Accueil</NavLink></li>
+                  <li><NavLink to='/faq' activeClassName="lien-actif"
+                               activeStyle={{color: "red", fontWeight: "bold"}}> F.A.Q </NavLink></li>
+                  <li><NavLink to='/cgv' activeClassName="lien-actif"
+                               activeStyle={{color: "red", fontWeight: "bold"}}> Conditions de vente </NavLink></li>
+               </ul>
+            </header>
+            <Switch>
 
-                    <Route path="/" component={Accueil} exact={true} />
-                    <Route path="/faq" component={Faq} />
-                    <Route path="/cgv" component={Cgv} />
-                    <Route path="*" component={Page404} />
+               <Route path="/" component={Accueil} exact={true}/>
+               <Route path="/faq" component={Faq}/>
+               <Route path="/cgv" component={Cgv}/>
+               <Route path="*" component={Page404}/>
 
-                </Switch>
-            </BrowserRouter>
-    );
-}
-export default App;
+            </Switch>
+         </BrowserRouter>
+      );
+   }
+
+   export default App;
