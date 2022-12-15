e565205691c4806e2f30837645e59293ce8554d0
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-15 17:30:03 -0500

    mise à jour

---
+++ 2022-12-15-03-app-crud-mongoDB/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.listen(3000, function() {
+   console.log('listening on 3000')
+})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/page1', (req, res) => {
+   res.sendFile(__dirname + '/page1.html')
+})
+
+app.get('/page2', (req, res) => {
+   res.sendFile(__dirname + '/page2.html')
+})
