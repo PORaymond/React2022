c0efa35b5f296f18f1d9f0ed1ae64f6b8350539f
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-15 11:04:45 -0500

    Ajouter des console.log pour identifier les sorties

--- 2022-12-08-03meshooks/src/components/Demohistory.js
+++ 2022-12-08-03meshooks/src/components/Demohistory.js
@@ -1,17 +1,19 @@
-import { useHistory } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import {useHistory} from 'react-router-dom';
+import {Container} from 'semantic-ui-react';
 
 const Demohistory = (props) => {
-    const history = useHistory();
-    console.log(history);
-    console.log(props);
+   const history = useHistory();
+   console.log("history :");
+   console.log(history);
+   console.log("props :");
+   console.log(props);
 
-    return (
-        <Container>
-            <h1>Demo USE HISTORY</h1>
-            <p>Bienvenue sur notre guide des pays, crée à partir de React, react-Router et l'APi restcountries.eu</p>
-    </Container>
-    );
+   return (
+      <Container>
+         <h1>Demo USE HISTORY</h1>
+         <p>Bienvenue sur notre guide des pays, crée à partir de React, react-Router et l'APi restcountries.eu</p>
+      </Container>
+   );
 };
 
 export default Demohistory;
