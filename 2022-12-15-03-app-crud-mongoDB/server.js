5ac59afe071eb6e97090864da429698ae8a50b5d
Olivier Raymond <olivier.raymond176@gmail.com>
2022-12-16 08:04:10 -0500

    Ajouter la connection à mongoDB

--- 2022-12-15-03-app-crud-mongoDB/server.js
+++ 2022-12-15-03-app-crud-mongoDB/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient
 
 app.listen(3000, function() {
    console.log('listening on 3000')
@@ -20,4 +21,12 @@ app.get('/page2', (req, res) => {
    res.sendFile(__dirname + '/page2.html')
 })
 
-app.post('/quotes', (req, res) => {console.log(req.body)})
+app.post('/quotes', (req, res) => {
+   console.log(req.body)
+})
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true})
+   .then(client =>{
+      console.log('Connected to DB');
+      const db = client.db('star-wars-quotes')
+   })
